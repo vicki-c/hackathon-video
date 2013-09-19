250917c0423e2e8b49a7e8e6b6f429d68dee3c77
Vicki Cheung <vicki@duolingo.com>
2013-09-19 04:40:56 -0400

    plugging in virtual keyboard

--- public/js/app.js
+++ public/js/app.js
@@ -160,6 +160,13 @@ App.SourceSegmentController = Ember.ObjectController.extend({
         },
         close : function() {
             this.set('focus', false);
+        },
+        toggleKeyboard : function() {
+            this.set('virtualKeyboard', !this.get('virtualKeyboard'));
+        },
+        onSoftKey : function(c) {
+            // BIG HACK
+            $('#translation').insertAtCaret(c);
         }
     },
     toggleNothingSaid : function() {
@@ -242,4 +249,32 @@ App.VideoPlayerView = Ember.View.extend({
         // ... more code ...
         player.play();
     }
+});
+
+jQuery.fn.extend({
+insertAtCaret: function(myValue){
+  return this.each(function(i) {
+    if (document.selection) {
+      //For browsers like Internet Explorer
+      this.focus();
+      var sel = document.selection.createRange();
+      sel.text = myValue;
+      this.focus();
+    }
+    else if (this.selectionStart || this.selectionStart == '0') {
+      //For browsers like Firefox and Webkit based
+      var startPos = this.selectionStart;
+      var endPos = this.selectionEnd;
+      var scrollTop = this.scrollTop;
+      this.value = this.value.substring(0, startPos)+myValue+this.value.substring(endPos,this.value.length);
+      this.focus();
+      this.selectionStart = startPos + myValue.length;
+      this.selectionEnd = startPos + myValue.length;
+      this.scrollTop = scrollTop;
+    } else {
+      this.value += myValue;
+      this.focus();
+    }
+  });
+}
 });
